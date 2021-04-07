465497a1ec7c58c608856a8838290dff221bc262
tsaristbomba <andrexerdna@gmail.com>
2021-04-07 18:03:06 -0300

    theme seo updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,11 @@
 module.exports = {
+  siteMetadata: {
+    title: `Luden`,
+    seoTitle: `Fast as fuk boi`,
+    description: `Luden is a great theme`,
+    author: `@tsaristbomba`,
+    logo: "ludenLogo.svg",
+    image: "/june.png",
+  },
   plugins: ["@teefe/gatsby-theme-luden"],
 };
